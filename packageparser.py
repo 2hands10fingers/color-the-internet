5dbb6bde26f234d8ceb58ce761a4e9becf04de8f
Tono <antonio.nogueras@yahoo.com>
2018-10-27 11:30:43 -0500

    Update packageparser.py

--- packageparser.py
+++ packageparser.py
@@ -4,92 +4,79 @@ import io
 from colors import rgb, hsv, hex, random
 import webbrowser
 
-motherobject = [] # behold the mother object
+motherobject = []  # behold the mother object
+packetfile = "/Users/antonionogueras/Desktop/innanet/colortheinnanet/packetz.json"
 
 # -------- functions #
-def calculatevariance(num, modulus, divide):
-    return (num - (num % modulus)) / divide
 
 # -- checking size of the time from last packet and converts it to velocity rate
+
+def calculatevariance(num, modulus, divide):
+		return (num - (num % modulus)) / divide
+
 def variancechecker(i):
-    if i == 1000000:
-        return 0.0095
-    elif i < 1000000 and i >= 100000:
-        return calculatevariance(i, 50000.0, 100000000.0)
-    elif i < 100000 and i >= 9500:
-        return 0.095
-    elif i < 9500 and i >= 1000:
-        return calculatevariance(i, 500.0, 100000.0)
-    elif i < 1000 and i >= 100:
-        return calculatevariance(i, 50.0, 1000.0)
-    elif i < 100 and i >= 5:
-        return calculatevariance(i, 5.0, 10.0)
-    elif i < 5 and i >= 0:
-        return 15
-    else:
-        return i
+		if i == 1000000:
+				return 0.0095
+		elif i < 1000000 and i >= 100000:
+				return calculatevariance(i, 50000.0, 100000000.0)
+		elif i < 100000 and i >= 9500:
+				return 0.095
+		elif i < 9500 and i >= 1000:
+				return calculatevariance(i, 500.0, 100000.0)
+		elif i < 1000 and i >= 100:
+				return calculatevariance(i, 50.0, 1000.0)
+		elif i < 100 and i >= 5:
+				return calculatevariance(i, 5.0, 10.0)
+		elif i < 5 and i >= 0:
+				return 15
+		else:
+				return i
 
 
 def rulename(proto):
-	if proto == "ARP":
-		return "#7faf4c"
-	elif proto == "Broadcast":
-		return "#00d9ff"
-	elif proto == "UDP":
-		return "#7c4caf"
-	elif proto == "TCP":
-		return "#FFFFFF"
-	elif proto == "TCP SYN/FIN":
-		return "#808080"
-	elif proto == "TCP RST":
-		return "#a9a9a9"
-	elif proto == "Bad TCP":
-		return "#ff2626"
-	elif proto == "HTTP":
-		return "#fff8dc"
-	elif proto == "ICMP":
-		return "#f19cd2"
-	elif proto == "SMB":
-		return "#ffb79a"
-	else:
-		return "nothing"
+	protodict = {"ARP": "#7faf4c", "Broadcast": "#00d9ff", "UDP": "#7c4caf", "TCP SYN/FIN": "#808080",
+							 "TCP RST": "#a9a9a9", "Bad TCP": "#ff2626", "HTTP": "#fff8dc", "ICMP": "#f19cd2",
+							 "SMB": "#ffb79a", "TCP": "#FFFFFF"}
+	return protodict.get(proto, "#000000")
+
 
 def tcplen(tcpsize, color):
-	if bool(tcpsize) == True and color == '#d7cd733c':
+	if bool(tcpsize) and color == '#d7cd733c':
 		return int(tcpsize) / 10
-	elif (bool(tcpsize) == True and color == '#a3f0daac'):
+	elif (bool(tcpsize) and color == '#a3f0daac'):
 		return int(tcpsize) / 10
 	else:
 		return 0
 
+
 # -------- parse json data #
-packetfile = "/Users/antonionogueras/Desktop/packetz.json"
+
+
 with open(packetfile, "r+") as file:
 	data = json.load(file)
 
 	for i in data:
 		try:
-			# - variables ------ #
-				# ----- json datapoints ------ #
-			timepoint = i["_source"]["layers"]["frame"]["frame.time_delta"]
-			sizepoint = i["_source"]["layers"]["frame"]["frame.len"]
-			colorpoint = i["_source"]["layers"]["eth"]["eth.dst"].split(':')
-			transparencypoint = i["_source"]["layers"]["eth"]["eth.src"]
-			protocol = i["_source"]["layers"]["frame"]["frame.coloring_rule.name"]
-			tcplength = i["_source"]["layers"]["tcp"]["tcp.len"]
+			source = i["_source"]["layers"]
+			timepoint = source["frame"]["frame.time_delta"]
+			sizepoint = source["frame"]["frame.len"]
+			colorpoint = source["eth"]["eth.dst"].split(':')
+			transparencypoint = source["eth"]["eth.src"]
+			protocol = source["frame"]["frame.coloring_rule.name"]
+			tcplength = source["tcp"]["tcp.len"]
 
-				# ----- colorpoint index ------ #
+			# ----- colorpoint index ------ #
 			firsthexsplit = colorpoint[0] + colorpoint[1] + colorpoint[2]
 			secondhexsplit = colorpoint[3] + colorpoint[4] + colorpoint[5]
 
-				# ----- make rgb index from converted hexcode ------ #
+			# ----- make rgb index from converted hexcode ------ #
 			torgb = hex(secondhexsplit).rgb
 			string_rgb = str(torgb).replace(" ", "").split(',')
 
 				# ----- variable conversions ------ #
 			blendcolors = hex(firsthexsplit).screen(rgb(int(string_rgb[0]),
-								    int(string_rgb[1]),
-								    int(string_rgb[2]))).hex
+										int(string_rgb[1]),
+										int(string_rgb[2]))).hex
 
 			parsecolor = "#{}{}".format(blendcolors, transparencypoint[0:2])
 			# print parsecolor
@@ -99,8 +86,7 @@ with open(packetfile, "r+") as file:
 					'size': (math.floor(int(sizepoint)) + (tcplen(tcplength, parsecolor))) / 55,
 					'proto': rulename(protocol),
 					'velocity': variancechecker(float(timepoint) * 1000000)
-				      }
-
+							}
 
 			# - append to the arrays ------ #
 			motherobject.append(babyobject)
@@ -108,7 +94,7 @@ with open(packetfile, "r+") as file:
 		except KeyError:
 			pass
 
-# -------- print to json #
+#-------- print to json #
 
 try:
     to_unicode = unicode
